d09fe460fe0778e4b0d9e8a04b8d6d54dd5256c3
ornichola <1502708+ornichola@users.noreply.github.com>
2021-05-29 18:18:08 +0300

    Test another product

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,7 @@
 from pages.product_page import ProductPage
 
 
-LINK = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+LINK = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
 
 
 def test_guest_can_add_product_to_basket(browser):
