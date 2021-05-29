da6830317e89633634e9eda500b156b44254fbbe
ornichola <1502708+ornichola@users.noreply.github.com>
2021-05-29 21:56:36 +0300

    Add few guest user tests for ProductPage

--- test_product_page.py
+++ test_product_page.py
@@ -15,7 +15,7 @@ LINK = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6',
     pytest.param(
         'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7',
-        marks=pytest.mark.xfail
+        marks=pytest.mark.xfail(reason='stepik-575-04-03-04')
     ),
     'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8',
     'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9'
@@ -26,3 +26,25 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_added_to_basket(product_name=page.product_name, product_price=page.product_price)
+
+
+@pytest.mark.skip(reason='stepik-575-04-03-06')
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, LINK)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, LINK)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail(reason='stepik-575-04-03-06')
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, LINK)
+    page.open()
+    page.add_to_basket()
+    page.should_fade_success_message()
