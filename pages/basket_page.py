2e363a71b7a37174dee1001daac6afbed6e378fe
ornichola <1502708+ornichola@users.noreply.github.com>
2021-05-30 01:47:33 +0300

    Describe BasketPage

---
+++ pages/basket_page.py
@@ -0,0 +1,11 @@
+from data import EMPTY_BASKET_MSG_BY_LANG
+from pages.base_page import BasePage
+from pages.locators import BasketPageLocators
+
+
+class BasketPage(BasePage):
+
+    def should_be_empty(self):
+        self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS)
+        empty_basket_msg = self.browser.find_element(*BasketPageLocators.EMPTY_BASKET_MSG).text
+        assert EMPTY_BASKET_MSG_BY_LANG.get(self.browser.language) in empty_basket_msg, 'Wrong empty basket message'
