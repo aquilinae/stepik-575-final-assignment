6ad87fa1150ac22344a18184f598f18f45105506
ornichola <1502708+ornichola@users.noreply.github.com>
2021-05-29 18:38:32 +0300

    Add parametrization for test_guest_can_add_product_to_basket

--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,13 @@
+import pytest
+
 from pages.product_page import ProductPage
 
+LINK = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
-LINK = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
 
-
-def test_guest_can_add_product_to_basket(browser):
-    page = ProductPage(browser, LINK)
+@pytest.mark.parametrize('link', [f'{LINK}?promo=offer{i}' for i in range(10)])
+def test_guest_can_add_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
