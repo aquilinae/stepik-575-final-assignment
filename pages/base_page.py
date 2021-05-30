df23aea3c52d9feb4a793d91aacbc8d8922c64f7
ornichola <1502708+ornichola@users.noreply.github.com>
2021-05-30 23:52:20 +0300

    Implement register_new_user method

--- pages/base_page.py
+++ pages/base_page.py
@@ -67,6 +67,9 @@ class BasePage:
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), 'Login link is not presented'
 
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), 'User icon is not presented, probably unauthorised user'
+
     def go_to_basket(self):
         basket_btn = self.browser.find_element(*BasePageLocators.VIEW_BASKET_BTN)
         basket_btn.click()
--- pages/locators.py
+++ pages/locators.py
@@ -5,6 +5,7 @@ class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     VIEW_BASKET_BTN = (By.XPATH, '//div[contains(@class, "basket")]//a')
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class MainPageLocators(BasePageLocators):
@@ -14,6 +15,10 @@ class MainPageLocators(BasePageLocators):
 class LoginPageLocators(BasePageLocators):
     LOGIN_FORM = (By.XPATH, '//form[@id="login_form"]')
     REGISTER_FORM = (By.XPATH, '//form[@id="register_form"]')
+    REGISTER_EMAIL = (By.XPATH, '//input[@id="id_registration-email"]')
+    REGISTER_PASSWORD = (By.XPATH, '//input[@id="id_registration-password1"]')
+    REGISTER_PASSWORD_CONFIRMATION = (By.XPATH, '//input[@id="id_registration-password2"]')
+    REGISTER_BUTTON = (By.XPATH, '//button[@name="registration_submit"]')
 
 
 class ProductPageLocators(BasePageLocators):
