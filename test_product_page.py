5826f557e09888b1b5991d63bfb1ebb07cc1122c
ornichola <1502708+ornichola@users.noreply.github.com>
2021-05-31 14:14:46 +0300

    Mark tests for final assignment with need_review pytest mark

--- test_product_page.py
+++ test_product_page.py
@@ -10,6 +10,7 @@ from pages.product_page import ProductPage
 LINK = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', [
     'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0',
     'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1',
@@ -62,12 +63,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, LINK)
     page.open()
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = ProductPage(browser, LINK)
     page.open()
@@ -94,6 +97,7 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         page = ProductPage(browser, LINK)
         page.open()
