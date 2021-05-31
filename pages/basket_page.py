8b3acdcef6a02d785f07d0b236a81f260697083c
ornichola <1502708+ornichola@users.noreply.github.com>
2021-05-31 14:14:05 +0300

    Add default eng message for should_be_empty method

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -8,4 +8,5 @@ class BasketPage(BasePage):
     def should_be_empty(self):
         self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS)
         empty_basket_msg = self.browser.find_element(*BasketPageLocators.EMPTY_BASKET_MSG).text
-        assert EMPTY_BASKET_MSG_BY_LANG.get(self.browser.language) in empty_basket_msg, 'Wrong empty basket message'
+        assert EMPTY_BASKET_MSG_BY_LANG.get(self.browser.language, 'Your basket is empty.') in empty_basket_msg, \
+            'Wrong empty basket message'
